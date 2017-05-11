c738cd9ad7065c7f79a8b1eb0ba1e5dacb1c97ea
Derrick Staten <statenderrick@gmail.com>
2017-05-11 14:42:38 -0700

    fix

--- web-sdk/beta.js
+++ web-sdk/beta.js
@@ -1996,11 +1996,11 @@ branch_view.handleBranchViewData = function(a, b, c, d, e, f, g) {
     var k = null, h = null;
     c = c || {};
     c.feature = "journeys";
-    // console.log('b: ', b)
-    // console.log('c: ', c)
-    // console.log('d: ', d)
-    // console.log('d.getAll(): ', d.getAll())
-    // c.link_click_id = utils.clickIdFromLink(g._referringLink());
+    console.log('b: ', b)
+    console.log('c: ', c)
+    console.log('d: ', d)
+    console.log('d.getAll(): ', d.getAll())
+    c.link_click_id = utils.clickIdFromLink(g._referringLink());
     c = utils.cleanLinkData(c);
     (g = document.getElementById("branch-iframe-css")) && g.parentElement.removeChild(g);
     var l = document.createElement("div");
